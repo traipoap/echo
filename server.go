ed0693cc698842daefb9f6d392d2c5a666bdbe90
traipoap <traipoap@hotmail.com>
2024-04-26 23:57:32 +0700

    Initial commit

---
+++ server.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"log"
+
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+
+	"gorm.io/driver/sqlite"
+
+	"gorm.io/gorm"
+)
+
+type CustomerHandler struct {
+	DB *gorm.DB // Field DB to Database
+}
+
+func (h *CustomerHandler) Initialize() {
+	db, err := gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.AutoMigrate(&Customer{})
+	h.DB = db
+}
+
+type Customer struct {
+	Id        uint   `gorm:"primary_key" json:"id"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Age       int    `json:"age"`
+	Email     string `json:"email"`
+}
+
+func (h *CustomerHandler) GetAllCustomer(c echo.Context) error {
+	customers := []Customer{}
+
+	h.DB.Find(&customers)
+
+	return c.JSON(http.StatusOK, customers)
+}
+
+func (h *CustomerHandler) GetCustomer(c echo.Context) error {
+	id := c.Param("id")
+	customer := Customer{}
+
+	if err := h.DB.Find(&customer, id).Error; err != nil {
+		return c.NoContent(http.StatusNotFound)
+	}
+
+	return c.JSON(http.StatusOK, customer)
+}
+
+func (h *CustomerHandler) SaveCustomer(c echo.Context) error {
+	customer := Customer{}
+
+	if err := c.Bind(&customer); err != nil {
+		return c.NoContent(http.StatusBadRequest)
+	}
+
+	if err := h.DB.Save(&customer).Error; err != nil {
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	return c.JSON(http.StatusOK, customer)
+}
+
+func (h *CustomerHandler) UpdateCustomer(c echo.Context) error {
+	id := c.Param("id")
+	customer := Customer{}
+
+	if err := h.DB.Find(&customer, id).Error; err != nil {
+		return c.NoContent(http.StatusNotFound)
+	}
+
+	if err := c.Bind(&customer); err != nil {
+		return c.NoContent(http.StatusBadRequest)
+	}
+
+	if err := h.DB.Save(&customer).Error; err != nil {
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	return c.JSON(http.StatusOK, customer)
+}
+
+func (h *CustomerHandler) DeleteCustomer(c echo.Context) error {
+	id := c.Param("id")
+	customer := Customer{}
+
+	if err := h.DB.Find(&customer, id).Error; err != nil {
+		return c.NoContent(http.StatusNotFound)
+	}
+
+	if err := h.DB.Delete(&customer).Error; err != nil {
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	return c.NoContent(http.StatusNoContent)
+}
+
+func main() {
+
+	e := echo.New()
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "time=${time_rfc3339}, remote_ip=${remote_ip}, method=${method}, host=${host}, uri=${uri}, status=${status}, error=${error}, latency_human=${latency_human}\n",
+	}))
+
+	h := CustomerHandler{}
+	h.Initialize()
+
+	e.GET("/customers", h.GetAllCustomer)
+	e.POST("/customers", h.SaveCustomer)
+	e.GET("/customers/:id", h.GetCustomer)
+	e.PUT("/customers/:id", h.UpdateCustomer)
+	e.DELETE("/customers/:id", h.DeleteCustomer)
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
