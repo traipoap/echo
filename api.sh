c19fee4f1d219ae95a5ece1c6d180ea86c2fbe54
traipoap <traipoap@hotmail.com>
2024-05-02 17:08:01 +0700

    Add "Request" api

--- api.sh
+++ api.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Loop to send 10 POST requests with incremented id
-for ((id=6086; id<=10000; id++)); do
+for ((id=1; id<=10; id++)); do
     curl --location -k 'https://echo.local.com/customers' \
     --header 'Content-Type: application/json' \
     --data-raw '{
         "id": '$id',
         "firstName": "echo",
         "lastName": "local.com",
-        "age": 29,
+        "age": 1,
         "email": "echo@local.com"
     }'
 done
