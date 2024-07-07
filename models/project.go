68d538398a9390f795222bdc4ea82ed0be1d6665
TRAIPOAP <109869423+traipoap@users.noreply.github.com>
2024-07-07 17:13:42 +0700

    Create project.go

---
+++ models/project.go
@@ -0,0 +1,11 @@
+// models/project.go
+package models
+
+type Project struct {
+	ID          int
+	Title       string
+	Year        int
+	Technology  string
+	Description string
+	ImageURL    string
+}
