c936920abe2183ebc1acb2d34b5f5efb6dd25328
traipoap <traipoap@hotmail.com>
2024-07-09 14:54:20 +0700

    Update routes.go

--- routes/routes.go
+++ routes/routes.go
@@ -9,10 +9,17 @@ import (
 
 func SetupRoutes(e *echo.Echo, pc *controllers.PortfolioController) {
 	e.GET("/", pc.Home)
+
 	e.GET("/works", pc.Works)
-	e.GET("/single-work", pc.Work)
+	e.GET("/project-1", pc.Project1)
+	e.GET("/project-2", pc.Project2)
+
+	e.GET("/experience", pc.Experience)
+
 	e.GET("/blog", pc.Blog)
+
 	e.GET("/about", pc.About)
+
 	e.GET("/stats", pc.GetVisitStats)
 
 	// Middleware for recording visits
