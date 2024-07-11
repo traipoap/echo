89575abda44016c4213c4d9ba325577eff12a4d9
TRAIPOAP <109869423+traipoap@users.noreply.github.com>
2024-07-11 19:16:16 +0700

    Update experience.go

--- models/experience.go
+++ models/experience.go
@@ -2,12 +2,11 @@
 package models
 
 type Experience struct {
-	ID         int
-	Title      string
-	Company    string
-	Year       string
-	Experience string
-	Ambition   string
-	Strength   string
-	Slug       string
+	ID              int
+	Title           string
+	Company         string
+	Year            string
+	Responsibilitie string
+	Skill           string
+	Achievement     string
 }
