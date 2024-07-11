c7890937158dc61ecb55bb363922113ff8418a99
traipoap <traipoap@hotmail.com>
2024-07-11 12:11:49 +0700

    Create experience.go

---
+++ models/experience.go
@@ -0,0 +1,12 @@
+package models
+
+type Experience struct {
+	ID         int
+	Title      string
+	Company    string
+	Year       string
+	Experience string
+	Ambition   string
+	Strength   string
+	Slug       string
+}
